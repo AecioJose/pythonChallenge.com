e603e310ea3f7284ae0bf3745048495815fc2f9e
Icaro <146675789+IcaroRubem@users.noreply.github.com>
2024-08-30 15:13:36 -0300

    Update level4.py

--- level4.py
+++ level4.py
@@ -1,4 +1,24 @@
-def discoverNothing():
-    #automate code to explore all nothings and save them in a txt file called nothingURLs
-    print("nada nao")
+from urllib.request import urlopen
+from re import findall
 
+urlDefault = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing="
+url = urlDefault + "12345"
+numbersFounds = []
+pdr = r"\d+"
+
+
+for n in range(400):
+    print(url)
+    html = urlopen(url=url)
+    text = html.read().decode("utf8")
+    numbers = findall(pdr, text)
+    numbersFounds.append(numbers)
+    if len(numbers) == 0:
+        print("\033[32mVERIFIQUE A URL:\033[m \n>>>", url)
+        break
+    if len(numbers) > 1:
+        number = numbers[1]
+    else:
+        number = numbers[0]
+    url = urlDefault + number
+print("\nNumeros Encontrados: \n", numbersFounds)
